89fd9502c6d9259a1023f087e186235c796ae0fd
Durmix <kacperdurmaj@gmail.com>
2021-05-14 16:48:47 +0200

    programShouldCallHeatingModuleTest

--- testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
+++ testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
@@ -1,18 +1,47 @@
 package edu.iis.mto.oven;
 
-import static org.hamcrest.Matchers.equalTo;
-
-import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 class OvenTest {
 
+    @Mock
+    private BakingProgram bakingProgram;
+    @Mock
+    private HeatingModule heatingModule;
+    @Mock
+    private HeatingSettings heatingSettings;
+    @Mock
+    private Fan fan;
+
+    private Oven oven;
+
+    @BeforeEach
+    void setUp() {
+        oven = new Oven(heatingModule, fan);
+    }
+
     @Test
-    void itCompiles() {
-        MatcherAssert.assertThat(true, equalTo(true));
+    void programShouldCallHeatingModuleWithGivenTemperatureAndTime() {
+        int targetTemp = 180;
+        int irrelevant = 50;
+        int time = 25;
+        heatingSettings = HeatingSettings.builder().withTargetTemp(targetTemp).withTimeInMinutes(time).build();
+        ProgramStage stageOne = ProgramStage.builder()
+                .withHeat(HeatType.HEATER).withTargetTemp(targetTemp).withStageTime(time).build();
+        List<ProgramStage> stages = List.of(stageOne);
+        bakingProgram = BakingProgram.builder()
+                .withInitialTemp(irrelevant).withStages(stages).build();
+        oven.start(bakingProgram);
+
+        Mockito.verify(heatingModule).heater(heatingSettings);
     }
 
 }
